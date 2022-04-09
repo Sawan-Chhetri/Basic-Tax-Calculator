39fe2b464f85e8ab851bd650a643eb1d639c1b48
sawanchhetri <chhetrisawan@hotmail.com>
2022-04-09 21:04:05 +1000

    Initial check-in of module ReadyTechAssesment

---
+++ Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ReadyTechAssessment
+{
+    public class Products
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsImported { get; set; }
+
+        public bool SalesTaxExempted { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ ReceiptData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ReadyTechAssessment
+{
+    public class ReceiptData : Products
+    {
+        public decimal SalesTax { get; set; }
+
+        public decimal ImportDuty { get; set; }
+
+        public decimal CheckOutItemPrice { get; set; }
+
+        public decimal CheckOutTotalPrice { get; set; }
+
+        public decimal TotalSalesTax { get; set; }
+
+        public decimal TotalTax { get; set; }
+    }
+}
